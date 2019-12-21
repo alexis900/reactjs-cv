cad027de31f7b903893045183d02076fb2bc72b0
Alejandro Martin Perez <alexandermpf77@gmail.com>
2019-12-22 00:36:31 +0100

    Updates

---
+++ precache-manifest.bb28ec009ed397ddd75662c050107645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3222d19907d206b2d2c6fe31b3e591ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf42adfad634a85be176",
+    "url": "./static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "815f21cf8b9104f03943",
+    "url": "./static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "bf42adfad634a85be176",
+    "url": "./static/js/2.751db1fc.chunk.js"
+  },
+  {
+    "revision": "9682237fa99bc1a358b1ac2de59fd99c",
+    "url": "./static/js/2.751db1fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "815f21cf8b9104f03943",
+    "url": "./static/js/main.42ce41f7.chunk.js"
+  },
+  {
+    "revision": "a87c729c0fc26ae8ae67",
+    "url": "./static/js/runtime-main.897b73e0.js"
+  }
+]);
